928cc9136567dbf144d3745fb3a8a7b4cc113e65
BrianAdams <brian@babastik.com>
2016-04-19 17:06:55 -0700

    Added support for 1080P30 and detection of SNOR bottomed

--- platform/linux/bootcamera.sh
+++ platform/linux/bootcamera.sh
@@ -17,14 +17,22 @@ then
     exit 1004
 fi
 
-awaitingBoot=$(mxcam whoami | grep -q 'Waiting for USB boot')
-
-if $awaitingBoot
+mxcam whoami | grep -q 'Waiting for USB boot'
+awaitingBoot=$?
+if [ $awaitingBoot -eq 0 ]
 then
     echo 'Booting camera'
-    mxcam boot $DIR/../../firmware/gc6500_ddrboot_fw.gz.img $DIR/../../geoconf/ov4689_H264_1080p24.json
+    mxcam boot $DIR/../../firmware/gc6500_ddrboot_fw.gz.img $DIR/../../geoconf/ov4689_H264_1080p30.json
 else
     echo 'Camera already booted'
+    mxcam bootmode | grep -qc 'snor'
+    bootmodesnor=$?
+    if [ $bootmodesnor -eq 0 ]
+    then
+      echo 'Camera is factor SNOR mode, changing to USB boot'
+     mxcam flash --bootloader --silent $DIR/../../firmware/gc6500_btld_ddrboot_534_epwr.rom
+     mxcam boot $DIR/../../firmware/gc6500_ddrboot_fw.gz.img $DIR/../../geoconf/ov4689_H264_1080p30.json
+    fi
 fi
 
 #videoDevice=$(file somefile | awk '{print $5}')
